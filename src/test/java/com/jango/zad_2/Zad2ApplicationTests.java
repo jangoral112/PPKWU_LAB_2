12a01375fdbf168441257c013bee0322c2fa5d63
Jan Góral <jan.goral@epassi.com>
2021-10-28 12:22:45 +0200

    Initial commit

---
+++ src/test/java/com/jango/zad_2/Zad2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jango.zad_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
