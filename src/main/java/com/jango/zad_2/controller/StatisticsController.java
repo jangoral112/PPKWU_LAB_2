ba21c7bbdaa73fb4cf7c51b3f38994f7dcd7e621
Jan Góral <jan.goral@epassi.com>
2021-10-28 12:54:52 +0200

    Added StringStatisticsRequest and StringStatisticsResponse to statistics flow

--- src/main/java/com/jango/zad_2/controller/StatisticsController.java
+++ src/main/java/com/jango/zad_2/controller/StatisticsController.java
@@ -1,9 +1,12 @@
 package com.jango.zad_2.controller;
 
+import com.jango.zad_2.dto.StringStatisticsRequest;
+import com.jango.zad_2.dto.StringStatisticsResponse;
 import com.jango.zad_2.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +16,7 @@ public class StatisticsController {
     private StatisticsService statisticsService;
 
     @PostMapping("/statistics}")
-    public ResponseEntity<String> stringStatistics() {
-        return ResponseEntity.ok(statisticsService.stringStatistics());
+    public ResponseEntity<StringStatisticsResponse> stringStatistics(@RequestBody StringStatisticsRequest request) {
+        return ResponseEntity.ok(statisticsService.stringStatistics(request));
     }
 }
--- src/main/java/com/jango/zad_2/service/StatisticsService.java
+++ src/main/java/com/jango/zad_2/service/StatisticsService.java
@@ -1,11 +1,13 @@
 package com.jango.zad_2.service;
 
+import com.jango.zad_2.dto.StringStatisticsRequest;
+import com.jango.zad_2.dto.StringStatisticsResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticsService {
 
-    public String stringStatistics() {
-        return "Not implemented yet";
+    public StringStatisticsResponse stringStatistics(StringStatisticsRequest request) {
+        return StringStatisticsResponse.builder().build();
     }
 }
